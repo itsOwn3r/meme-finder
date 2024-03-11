87c2bb1ec842c7f5b563654b9fed27a816678ab7
Own3r <darkwebsite95@gmail.com>
2024-03-11 12:43:17 +0330

    input:file added  and preview of meme is shown to the user

--- app/new/page.tsx
+++ app/new/page.tsx
@@ -1,10 +1,21 @@
-import Button from '@/components/Button/Button'
+"use client";
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 import { GoArrowLeft } from 'react-icons/go'
 import Image from "next/image";
 
 const NewMemePage = () => {
+    const [meme, setMeme] = useState<File | undefined>(undefined);
+
+    const memeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+        console.log(e);
+        if (e.target.files) {
+            console.log(e.target.files[0]);
+            setMeme(e.target.files[0])
+        }
+    }
+    console.log(meme);
+
   return (
     <section className="mt-8">
     <Link scroll={false} href="/" className="flex items-center gap-x-1 pl-2">
@@ -12,9 +23,21 @@ const NewMemePage = () => {
       <span className=" font-semibold text-text text-base">Back</span>
     </Link>
     <div className="flex flex-col md:flex-row mt-5 gap-x-10 lg:gap-x-16 justify-center items-center">
-      <div className=" bg-super-dark-gray flex relative items-center justify-center md:pb-[25%] md:pt-[25%] pt-[50%] pb-[50%] rounded-2xl w-full md:w-1/2 border-4 border-outline border-opacity-15">
-        <div className="absolute w-2/3 rounded-3xl overflow-hidden  shadow-shine bg-transparent bg-opacity-0 ">
-            Enter Meme Here
+      <div className="bg-super-dark-gray flex relative items-center justify-center md:pb-[15%] md:pt-[15%] pt-[50%] pb-[50%] rounded-2xl w-full md:w-1/2 border-4 border-outline border-opacity-15">
+        <div className="absolute w-2/3 rounded-3xl overflow-hidden">
+            <div className='w-full relative h-[8rem] p-40 flex justify-center items-center cursor-pointer'>
+                {meme && (
+                        <>
+                            <Image src={URL.createObjectURL(meme)} fill alt='Meme' />
+                            <input onChange={e => memeHandler(e)} className='absolute w-full h-full z-50 opacity-0 cursor-pointer' type="file" name="meme" id="meme" />
+                        </>
+                )}
+                {!meme && <>
+                    <div className='w-full relative h-[8rem] flex justify-center cursor-pointer'><Image className='absolute rounded-full cursor-pointer' src="/images/add-icon.png" width={120} height={120} alt='Add meme' quality={100} />
+                    <input onChange={e => memeHandler(e)} className='z-50 opacity-0 cursor-pointer' type="file" name="meme" id="meme" />
+                    </div>
+                </>}
+            </div>
         </div>
       </div>
       <div className="w-full mt-12 md:mt-0 md:w-1/2">
