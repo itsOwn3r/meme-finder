47a059ae5206e8bf83789d6eb517e7ed6eefeacd
Own3r <darkwebsite95@gmail.com>
2024-03-11 08:32:06 +0330

    logo for mobile and desktop added

--- components/Header/Navbar.jsx
+++ components/Header/Navbar.jsx
@@ -1,6 +1,5 @@
 import Button from "../Button/Button";
-import { LogoFull } from "../SVGs/Logofull";
-import { LogoSimple } from "../SVGs/Logosimple";
+import MemeFinderLogo from "../SVGs/memeFinderLogo";
 import Link from "next/link";
 
 export default function Navbar() {
@@ -8,8 +7,11 @@ export default function Navbar() {
     <header className=" border-b-2 border-b-dim-gray border-opacity-30">
       <nav className="px-0 sm:px-6 pt-1 pb-2 xl:pb-4 2xl:pb-6 border-gray border-opacity-20 mt-4 xl:mt-8 2xl:mt-10  flex items-center justify-between">
         <Link aria-label="Back to Home" href="/">
-          <LogoSimple />
-          <LogoFull />
+            <MemeFinderLogo className="md:hidden block" />
+          <div className="hidden items-center md:flex xl:w-72 2xl:w-96">
+            <MemeFinderLogo />
+            <h2 className="text-h4 ml-3">Meme Finder</h2>
+          </div>
         </Link>
         <div className="flex items-center gap-x-8">
           <Link className="font-medium hidden sm:block relative overflow-hidden group h-fit text-base xl:text-h6 2xl:text-h5" href="/about">
