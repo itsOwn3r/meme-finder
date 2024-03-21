7a311344e272944b0f556547395c903b23dc2552
Own3r <darkwebsite95@gmail.com>
2024-03-21 11:52:27 +0330

    pwa added

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import localFont from "next/font/local";
 import Navbar from "@/components/Header/Navbar";
 import Footer from "@/components/Footer/Footer";
+import { Viewport } from "next";
 
 const overusedgrotesk = localFont({
   src: [
@@ -13,19 +14,55 @@ const overusedgrotesk = localFont({
   variable: "--font-overusedgrotesk",
 });
 
+const APP_NAME = "Meme Finder";
+const APP_DEFAULT_TITLE = "Meme Finder";
+const APP_TITLE_TEMPLATE = "%s - Meme Finder";
+const APP_DESCRIPTION = "A great project to keep track of your Memes!";
+
 export const metadata = {
   metadataBase: new URL("https://meme.own3r.me"),
-  alternates: {
-    canonical: "/",
-  },
-  title: "Meme Finder",
-  description:
-    "A great project to keep track of your Memes!",
   robots: {
     index: true,
     follow: true,
     nocache: true,
   },
+  applicationName: APP_NAME,
+  title: {
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE,
+  },
+  description: APP_DESCRIPTION,
+  manifest: "/manifest.json",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: APP_DEFAULT_TITLE,
+    // startUpImage: [],
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: "website",
+    siteName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+  twitter: {
+    card: "summary",
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+};
+
+export const viewport: Viewport = {
+  themeColor: "#0c0d0e",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,9 @@
+import {
+  PHASE_DEVELOPMENT_SERVER,
+  PHASE_PRODUCTION_BUILD,
+} from "next/constants.js";
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // reactStrictMode: true,
@@ -20,4 +26,16 @@ const nextConfig = {
     },
 };
 
-export default nextConfig;
+// export default nextConfig;
+
+const nextConfigFunction = async (phase) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD) {
+    const withPWA = (await import("@ducanh2912/next-pwa")).default({
+      dest: "public",
+    });
+    return withPWA(nextConfig);
+  }
+  return nextConfig;
+};
+
+export default nextConfigFunction;
