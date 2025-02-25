4c3eeee3b53d0fb24768576a4cc6d135efa9983e
Own3r <darkwebsite95@gmail.com>
2025-02-25 19:29:12 +0330

    fixed typo in adding new meme from threads, added saving catch message to db

--- app/api/new/threads/route.ts
+++ app/api/new/threads/route.ts
@@ -1,16 +1,25 @@
+import { ObjToSendType } from "@/app/new/page";
+import db from "@/db";
 import { NextRequest, NextResponse } from "next/server";
 
 
 export async function POST(req: NextRequest){
+    const url = [];
+
     try {
         const formData = await req.formData();
 
-        const sendToWrapper = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/new/threads/wrapper`, {
+        const sendToWrapper = await fetch(`${process.env.NEXT_PUBLIC_URL}api/new/threads/wrapper`, {
             method: "POST",
             body: formData
         })
 
         const data = await sendToWrapper.json();
+
+        const body: ObjToSendType = JSON.parse(formData.get("body") as string);
+
+        url.push(body.url);
+        
         
         if (data?.success) {
             return NextResponse.json({ success: true });
@@ -19,6 +28,12 @@ export async function POST(req: NextRequest){
         }
         
     } catch (error) {
+        await db.errors.create({
+            data: {
+                title: (error as Error).message,
+                url: url.length > 0 ? url[0] as string: "No title"
+            }
+        })
         return NextResponse.json({ message: (error as Error).message, success: false});
         
     }
